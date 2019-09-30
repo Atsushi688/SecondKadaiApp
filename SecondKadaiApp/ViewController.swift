205e703cc309ebaa38a5ad0469110bb89a3e40b7
Atsushi.Fujishita <fwii188026@icloud.com>
2019-09-30 21:57:56 +0900

    完成した。

--- SecondKadaiApp/ViewController.swift
+++ SecondKadaiApp/ViewController.swift
@@ -9,12 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var textlavel: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        let resultViewController:ResultViewController = segue.destination as! ResultViewController
+        resultViewController.name = textlavel.text!
+        
+    }
+    
+    
+    
 }
 
